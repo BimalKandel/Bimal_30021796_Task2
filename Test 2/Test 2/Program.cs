5e49e807932e5068d1d4c3bae7b6921a7cf9e0d2
Your Name <you@example.com>
2019-09-25 11:01:46 +1200

    msg

---
+++ Test 2/Test 2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            salary s1 = new salary();
+            s1.AnnualSalary = 80000;
+            Console.WriteLine($"Your salary is se tat {s1.AnnualSalary}");
+            Console.WriteLine($"your salary per weekis {s1.DispalySalary()}");
+            Console.WriteLine("");
+            Console.WriteLine("i will calculate your wages");
+            Console.WriteLine("enter a number of hour work:");
+            int hour = int.Parse(Console.ReadLine());
+        }
+        public class salary
+        {
+            double annualsalary;
+            double weekelysalary;
+
+            public double Annualsalary
+            {
+                get { return annualsalary; }
+                set { annualsalary = value; }
+
+                public double Weekelysalary
+            {
+                get { return weekelysalary; }
+                set { Weekelysalary = value; }
+
+            }
+
+            public salary()
+            {
+                Weekelysalary = DisplaySalary();
+
+            }
+            public double DisplaySalary(); 
+            {
+
+            return (annualsalary* 7) / 365;
+             }
+    }
+    public class wages
+    {
+        double numhours;
+        double weekelywages;
+
+    }
+
+    public double NUmHours
+    {
+        get { return this.NumHour}
+        set { this.NumHours = value}
+    }
+
+    public double Weekelywages
+    {
+        get { return this.weekelywage; }
+        set { weekely wages = Value}
+
+    }
+    get { return this.NumHour}
+    set { this.NumHours = value}
+
+
+    public double DisplaySalary(); 
+            {
+
+            return (annualsalary* 7) / 365;
+
+
+
+
+
+
+
+
+
+
+
+
